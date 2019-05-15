3e68805b46985aa15c7864a5135bc3d0e6649383
Juan Camilo Cortés Medina <jcamilo.36@gmail.com>
2019-05-15 16:01:09 -0500

    feat: tags and categories

--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,55 @@
+from django import forms
 from django.db import models
+
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.tags import ClusterTaggableManager
+
+from taggit.models import TaggedItemBase, Tag as TaggitTag
+
 from wagtail.core.models import Page
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.fields import RichTextField
+from wagtail.snippets.models import register_snippet
 
 class BlogPage(Page):
     description = models.CharField(max_length=255, blank=True,)
 
     content_panels = Page.content_panels + [
-        FieldPanel("description", classname="full")
+        FieldPanel("description", classname = "full")
     ]
 
 class PostPage(Page):
     body = RichTextField(blank=True)
+    categories = ParentalManyToManyField("blog.BlogCategory", blank=True)
+    tags = ClusterTaggableManager(through="blog.BlogPageTag", blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel("body", classname="full")
+        FieldPanel("body", classname = "full"),
+        FieldPanel("categories", widget=forms.CheckboxSelectMultiple),
+        FieldPanel("tags"),
     ]
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey('PostPage', related_name = "post_tags")
+
+@register_snippet
+class Tag(TaggitTag):
+    class Meta:
+        proxy = True
+
+@register_snippet
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=80)
+
+    panels = [
+        FieldPanel("name"),
+        FieldPanel("slug")
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
